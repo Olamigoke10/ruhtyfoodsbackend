e3e05e22f78d07c4b46a11fe209852cd18dd0482
MRBOX <olamigokeadebayo90@gmail.com>
2025-09-02 01:28:40 +0100

    setip for backend

---
+++ ruthybackend/auth/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}" if self.first_name else self.username
